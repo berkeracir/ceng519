ba50a4f2e5a6fd5cebfd82b36a2c6da53909162c
berkeracir <berkeracir159@gmail.com>
2022-06-16 01:35:55 +0300

    add plot files together with their outputs

---
+++ results/plot_all.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+plt.rcParams["figure.figsize"] = [8, 4]
+plt.rcParams["figure.autolayout"] = True
+
+titles = ['Vectorized Implementation', 'Parallelized Implementation', 'Parallelized Implementation without Loops']
+reduce_files = ['vectorized_reduce.csv', 'parallel_reduce.csv', 'parallel_loop_reduce.csv']
+distances_files = ['vectorized_distances.csv', 'parallel_distances.csv', 'parallel_loop_distances.csv']
+
+os.makedirs('plot_all', exist_ok=True)	# create directory if not exists
+dir_name = 'plot_all/'
+
+# REDUCE FILES
+for i in range(len(reduce_files)):
+	reduce_file = reduce_files[i]
+	title = titles[i]
+
+	# Header: n VecSize sim CompileTime KeyGenerationTime EncryptionTime ExecutionTime DecryptionTime ReferenceExecutionTime MSE
+	df = pd.read_csv(reduce_file).drop(columns=['sim'])
+	df['n'] = df['n'] * df['n']
+	gb = df.groupby(['n','VecSize'])
+	gb_values = list(gb.groups)
+	mean = gb.mean()
+	std = gb.std()
+	x_pos = np.arange(len(gb_values))
+
+	plt.figure(figsize=(1 * len(gb_values), 4))
+
+	width = 0.15
+	bar1 = plt.bar(x_pos - width*2.5, mean['CompileTime'], width, yerr=std['CompileTime'], align='center', capsize=3)
+	bar2 = plt.bar(x_pos - width*1.5, mean['KeyGenerationTime'], width, yerr=std['KeyGenerationTime'], align='center', capsize=3)
+	bar3 = plt.bar(x_pos - width*0.5, mean['EncryptionTime'], width, yerr=std['EncryptionTime'], align='center', capsize=3)
+	bar4 = plt.bar(x_pos + width*0.5, mean['ExecutionTime'], width, yerr=std['ExecutionTime'], align='center', capsize=3)
+	bar5 = plt.bar(x_pos + width*1.5, mean['DecryptionTime'], width, yerr=std['DecryptionTime'], align='center', capsize=3)
+	bar6 = plt.bar(x_pos + width*2.5, mean['ReferenceExecutionTime'], width, yerr=std['ReferenceExecutionTime'], align='center', capsize=3)
+	bars = [bar1, bar2, bar3, bar4, bar5, bar6]
+	bar_names = ['Compile', 'Key Generation', 'Encryption', 'Execution', 'Decryption', 'Reference Execution']
+
+	plt.xlabel('Input Matrix Size, Vector Size')
+	plt.ylabel('Time (ms)')
+	plt.title(f'Times for Reduced Ones ({title})')
+	plt.xticks(x_pos, gb_values, rotation = 0)
+	plt.legend(bars, bar_names, loc='upper left')
+	plt.xlim(x_pos[0] - width*3, x_pos[-1] + width*3)
+	plt.ylim(0,100)
+	plt.savefig(dir_name + reduce_file.replace('.csv', '_times.png'))
+	plt.clf()	# clear the saved figure
+
+	if i != 0:
+		plt.figure(figsize=(0.7 * len(gb_values), 4))
+
+	width = 0.8
+	bar_mse = plt.bar(x_pos, mean['MSE'], width, yerr=std['MSE'], align='center', capsize=3, color='indianred')
+
+	plt.xlabel('Input Matrix Size, Vector Size')
+	plt.ylabel('MSE Score')
+	plt.title(f'MSE Scores for Reduce Ones ({title})')
+	plt.xticks(x_pos, gb_values, rotation = 0)
+	plt.xlim(x_pos[0] - width/2, x_pos[-1] + width/2)
+	plt.ylim(bottom=0)
+	plt.savefig(dir_name + reduce_file.replace('.csv', '_mse.png'))
+
+# DISTANCES FILES
+for i in range(len(distances_files)):
+	distances_file = distances_files[i]
+	title = titles[i]
+
+	# Header: n VecSize sim CompileTime KeyGenerationTime EncryptionTime ExecutionTime DecryptionTime ReferenceExecutionTime MSE
+	df = pd.read_csv(distances_file).drop(columns=['sim'])
+	df['n'] = df['n'] * df['n']
+	gb = df.groupby(['n','VecSize'])
+	gb_values = list(gb.groups)
+	mean = gb.mean()
+	std = gb.std()
+	x_pos = np.arange(len(gb_values))
+
+	plt.figure(figsize=(1 * len(gb_values), 4))
+
+	width = 0.15
+	bar1 = plt.bar(x_pos - width*2.5, mean['CompileTime'], width, yerr=std['CompileTime'], align='center', capsize=3)
+	bar2 = plt.bar(x_pos - width*1.5, mean['KeyGenerationTime'], width, yerr=std['KeyGenerationTime'], align='center', capsize=3)
+	bar3 = plt.bar(x_pos - width*0.5, mean['EncryptionTime'], width, yerr=std['EncryptionTime'], align='center', capsize=3)
+	bar4 = plt.bar(x_pos + width*0.5, mean['ExecutionTime'], width, yerr=std['ExecutionTime'], align='center', capsize=3)
+	bar5 = plt.bar(x_pos + width*1.5, mean['DecryptionTime'], width, yerr=std['DecryptionTime'], align='center', capsize=3)
+	bar6 = plt.bar(x_pos + width*2.5, mean['ReferenceExecutionTime'], width, yerr=std['ReferenceExecutionTime'], align='center', capsize=3)
+	bars = [bar1, bar2, bar3, bar4, bar5, bar6]
+	bar_names = ['Compile', 'Key Generation', 'Encryption', 'Execution', 'Decryption', 'Reference Execution']
+
+	plt.xlabel('Input Matrix Size, Vector Size')
+	plt.ylabel('Time (ms)')
+	plt.title(f'Times for Compute Distances ({title})')
+	plt.xticks(x_pos, gb_values, rotation = 0)
+	location = (0.01,0.55)
+	plt.legend(bars, bar_names, loc='upper left')
+	plt.xlim(x_pos[0] - width*3, x_pos[-1] + width*3)
+	if i == 0:
+		plt.ylim(0,100)
+	else:
+		plt.ylim(bottom=0)
+	plt.savefig(dir_name + distances_file.replace('.csv', '_times.png'))
+	plt.clf()	# clear the saved figure
+
+	if i != 0:
+		plt.figure(figsize=(0.7 * len(gb_values), 4))
+
+	width = 0.8
+	bar_mse = plt.bar(x_pos, mean['MSE'], width, yerr=std['MSE'], align='center', capsize=3, color='indianred')
+
+	plt.xlabel('Input Matrix Size, Vector Size')
+	plt.ylabel('MSE Score')
+	plt.title(f'MSE Scores for Compute Distances ({title})')
+	plt.xticks(x_pos, gb_values, rotation = 0)
+	plt.xlim(x_pos[0] - width/2, x_pos[-1] + width/2)
+	plt.ylim(bottom=0)
+	plt.savefig(dir_name + distances_file.replace('.csv', '_mse.png'))
---
+++ results/plot_inputsize.py
@@ -0,0 +1,152 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+labels = ['Vectorized', 'Parallelized', 'P. w/o. Loops']
+reduce_files = ['vectorized_reduce.csv', 'parallel_reduce.csv', 'parallel_loop_reduce.csv']
+distances_files = ['vectorized_distances.csv', 'parallel_distances.csv', 'parallel_loop_distances.csv']
+
+os.makedirs('plot_by_inputsize', exist_ok=True)	# create directory if not exists
+dir_name = 'plot_by_inputsize/'
+
+width = 0.3
+# REDUCE TIMINGS
+fig_reduce, axs_reduce = plt.subplots(nrows=2, ncols=3, figsize=(12,6), constrained_layout=True)
+fig_reduce.suptitle('Times for Reduce Ones EVA Program')
+
+# REDUCE FILES
+for i in range(len(reduce_files)):
+	distance_file = reduce_files[i]
+
+	# Header: n VecSize sim CompileTime KeyGenerationTime EncryptionTime ExecutionTime DecryptionTime ReferenceExecutionTime MSE
+	df = pd.read_csv(distance_file).drop(columns=['sim'])
+	if i == 0:
+		df = df[(df['n'] == 1) | (df['n'] == 2) | (df['n'] == 3)]
+	else:
+		df = df[((df['n'] == 1) | (df['n'] == 2) | (df['n'] == 3)) & (df['VecSize'] == 1)]
+
+	df['n'] = df['n'] * df['n']
+	gb = df.groupby(['n'])
+	gb_values = list(gb.groups)
+	mean = gb.mean()
+	std = gb.std()
+	x_pos = np.arange(len(gb_values))
+
+	axs_reduce[0,0].bar(x_pos + width*(i-1), mean['CompileTime'], width, yerr=std['CompileTime'], align='center', capsize=3, label=labels[i])
+	axs_reduce[0,1].bar(x_pos + width*(i-1), mean['KeyGenerationTime'], width, yerr=std['KeyGenerationTime'], align='center', capsize=3, label=labels[i])
+	axs_reduce[0,2].bar(x_pos + width*(i-1), mean['EncryptionTime'], width, yerr=std['EncryptionTime'], align='center', capsize=3, label=labels[i])
+	axs_reduce[1,0].bar(x_pos + width*(i-1), mean['ExecutionTime'], width, yerr=std['ExecutionTime'], align='center', capsize=3, label=labels[i])
+	axs_reduce[1,1].bar(x_pos + width*(i-1), mean['DecryptionTime'], width, yerr=std['DecryptionTime'], align='center', capsize=3, label=labels[i])
+	axs_reduce[1,2].bar(x_pos + width*(i-1), mean['ReferenceExecutionTime'], width, yerr=std['ReferenceExecutionTime'], align='center', capsize=3, label=labels[i])
+
+axs_reduce[0, 0].set_title('Compile Times')
+axs_reduce[0, 1].set_title('Key Generation Times')
+axs_reduce[0, 2].set_title('Encryption Times')
+axs_reduce[1, 0].set_title('Execution Times')
+axs_reduce[1, 1].set_title('Decryption Times')
+axs_reduce[1, 2].set_title('Reference Execution Times')
+
+for i in range(2):
+	for j in range(3):
+		axs_reduce[i,j].legend(loc='best')
+plt.setp(axs_reduce, xlabel='Input Matrix Size', ylabel='Time (ms)', xticks=x_pos, xticklabels=gb_values, xlim=(x_pos[0]-1.5*width, x_pos[-1]+1.5*width))
+# plt.show()
+plt.savefig(dir_name + 'reduce_times.png')
+plt.clf()	# clear the saved figure
+
+# DISTANCES TIMINGS
+fig_distances, axs_distances = plt.subplots(nrows=2, ncols=3, figsize=(12,6), constrained_layout=True)
+fig_distances.suptitle('Times for Compute Distances EVA Program')
+
+# DISTANCES FILES
+for i in range(len(distances_files)):
+	distance_file = distances_files[i]
+
+	# Header: n VecSize sim CompileTime KeyGenerationTime EncryptionTime ExecutionTime DecryptionTime ReferenceExecutionTime MSE
+	df = pd.read_csv(distance_file).drop(columns=['sim'])
+	if i == 0:
+		df = df[(df['n'] == 1) | (df['n'] == 2) | (df['n'] == 3)]
+	else:
+		df = df[((df['n'] == 1) | (df['n'] == 2) | (df['n'] == 3)) & (df['VecSize'] == 1)]
+
+	df['n'] = df['n'] * df['n']
+	gb = df.groupby(['n'])
+	gb_values = list(gb.groups)
+	mean = gb.mean()
+	std = gb.std()
+	x_pos = np.arange(len(gb_values))
+
+	axs_distances[0,0].bar(x_pos + width*(i-1), mean['CompileTime'], width, yerr=std['CompileTime'], align='center', capsize=3, label=labels[i])
+	axs_distances[0,1].bar(x_pos + width*(i-1), mean['KeyGenerationTime'], width, yerr=std['KeyGenerationTime'], align='center', capsize=3, label=labels[i])
+	axs_distances[0,2].bar(x_pos + width*(i-1), mean['EncryptionTime'], width, yerr=std['EncryptionTime'], align='center', capsize=3, label=labels[i])
+	axs_distances[1,0].bar(x_pos + width*(i-1), mean['ExecutionTime'], width, yerr=std['ExecutionTime'], align='center', capsize=3, label=labels[i])
+	axs_distances[1,1].bar(x_pos + width*(i-1), mean['DecryptionTime'], width, yerr=std['DecryptionTime'], align='center', capsize=3, label=labels[i])
+	axs_distances[1,2].bar(x_pos + width*(i-1), mean['ReferenceExecutionTime'], width, yerr=std['ReferenceExecutionTime'], align='center', capsize=3, label=labels[i])
+
+axs_distances[0, 0].set_title('Compile Times')
+axs_distances[0, 1].set_title('Key Generation Times')
+axs_distances[0, 2].set_title('Encryption Times')
+axs_distances[1, 0].set_title('Execution Times')
+axs_distances[1, 1].set_title('Decryption Times')
+axs_distances[1, 2].set_title('Reference Execution Times')
+
+for i in range(2):
+	for j in range(3):
+		axs_distances[i,j].legend(loc='best')
+plt.setp(axs_distances, xlabel='Input Matrix Size', ylabel='Time (ms)', xticks=x_pos, xticklabels=gb_values, xlim=(x_pos[0]-1.5*width, x_pos[-1]+1.5*width))
+plt.savefig(dir_name + 'distances_times.png')
+plt.clf()	# clear the saved figure
+
+# MSE
+fig_mse, axs_mse = plt.subplots(nrows=1, ncols=2, figsize=(8,4), constrained_layout=True)
+fig_mse.suptitle('MSE Scores of EVA Programs')
+
+# REDUCE FILES
+for i in range(len(reduce_files)):
+	reduce_file = reduce_files[i]
+
+	# Header: n VecSize sim CompileTime KeyGenerationTime EncryptionTime ExecutionTime DecryptionTime ReferenceExecutionTime MSE
+	df = pd.read_csv(reduce_file).drop(columns=['sim'])
+	if i == 0:
+		df = df[(df['n'] == 1) | (df['n'] == 2) | (df['n'] == 3)]
+	else:
+		df = df[((df['n'] == 1) | (df['n'] == 2) | (df['n'] == 3)) & (df['VecSize'] == 1)]
+
+	df['n'] = df['n'] * df['n']
+	gb = df.groupby(['n'])
+	gb_values = list(gb.groups)
+	mean = gb.mean()
+	std = gb.std()
+	x_pos = np.arange(len(gb_values))
+
+	axs_mse[0].bar(x_pos + width*(i-1), mean['MSE'], width, yerr=std['MSE'], align='center', capsize=3, label=labels[i])
+
+# DISTANCE FILES
+for i in range(len(distances_files)):
+	distance_file = distances_files[i]
+
+	# Header: n VecSize sim CompileTime KeyGenerationTime EncryptionTime ExecutionTime DecryptionTime ReferenceExecutionTime MSE
+	df = pd.read_csv(distance_file).drop(columns=['sim'])
+	if i == 0:
+		df = df[(df['n'] == 1) | (df['n'] == 2) | (df['n'] == 3)]
+	else:
+		df = df[((df['n'] == 1) | (df['n'] == 2) | (df['n'] == 3)) & (df['VecSize'] == 1)]
+
+	df['n'] = df['n'] * df['n']
+	gb = df.groupby(['n'])
+	gb_values = list(gb.groups)
+	mean = gb.mean()
+	std = gb.std()
+	x_pos = np.arange(len(gb_values))
+
+	axs_mse[1].bar(x_pos + width*(i-1), mean['MSE'], width, yerr=std['MSE'], align='center', capsize=3, label=labels[i])
+
+axs_mse[0].set_title('Reduce Ones EVA Program')
+axs_mse[1].set_title('Compute Distances EVA Program')
+
+for i in range(2):
+	axs_mse[i].legend(loc='upper right')
+	axs_mse[i].set_ylim(bottom=0)
+plt.setp(axs_mse, xlabel='Input Matrix Size', ylabel='MSE Score', xticks=x_pos, xticklabels=gb_values, xlim=(x_pos[0]-1.5*width, x_pos[-1]+1.5*width))
+plt.savefig(dir_name + 'mse.png')
